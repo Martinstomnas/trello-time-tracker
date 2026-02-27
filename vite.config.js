409e049d77ad5e47ca24be9940a6dab0a467407d
Martin Stomnås <144673018+Martinstomnas@users.noreply.github.com>
2026-02-27 12:47:55 +0100

    Removed unused import

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react";
 import { resolve } from "path";
-import { readFileSync, writeFileSync, mkdirSync } from "fs";
+import { readFileSync, writeFileSync } from "fs";
 import basicSsl from "@vitejs/plugin-basic-ssl";
 
 /**
